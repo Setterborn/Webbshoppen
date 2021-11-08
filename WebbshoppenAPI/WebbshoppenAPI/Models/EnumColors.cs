26d3dbeacb4dfd6ff1e17e29c6006edc63de4a19
Setterborn <90314260+Setterborn@users.noreply.github.com>
2021-11-08 12:56:03 +0100

    updaterat api

---
+++ WebbshoppenAPI/WebbshoppenAPI/Models/EnumColors.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebbshoppenAPI.Models
+{
+    public enum Colors
+    {
+        Black,
+        Red,
+        Yellow,
+        Blue
+    }
+    
+}
---
+++ WebbshoppenAPI/WebbshoppenAPI/Models/Shoes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebbshoppenAPI.Models
+{
+    public class Shoes : Product
+    {
+        public int Size { get; set; }
+        public Colors Colors { get; set; }
+        public Gender Gender { get; set; }
+
+        public Shoes(int size, Colors color, Gender gender, string name, double price, string description, int stock, string imageLink)
+        {
+            Size = size;
+            Colors = color;
+            Gender = gender;
+            Name = name;
+            Price = price;
+            Description = description;
+            Stock = stock;
+            ImageLink = imageLink;
+        }
+    }
+}
