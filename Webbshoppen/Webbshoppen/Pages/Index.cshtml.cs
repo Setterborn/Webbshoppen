a6e85842df9e57411fdc63e141cfdc161abe6dbb
almafrank <92314215+almafrank@users.noreply.github.com>
2021-11-09 21:58:35 +0100

    Index uppdaterat

--- Webbshoppen/Webbshoppen/Pages/Index.cshtml.cs
+++ Webbshoppen/Webbshoppen/Pages/Index.cshtml.cs
@@ -5,21 +5,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+//Skriva class som det ska hämtas från!
 
 namespace Webbshoppen.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        /* public List<Model.Product> Products { get; set; }
+         public IEnumerable<Model.Product> FindProduct { get; set; }
+         
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+         [BindProperty]
+         public string  Shirt { get; set; }//stor eller liten bokstav på property?
 
+         public string Pants { get; set; }
+
+         public string Shoe {get; set;}       
+         public string Underwear { get; set; }
+         public string Jacket { get; set; }
+         public string Female { get; set; }
+         public string Male { get; set; }
+         public string Unisex { get; set; }
+         public string Brand { get; set; }*/
+
+
+        public string ShopName { get; set; }
+        
         public void OnGet()
         {
-
+            ShopName = "Comfortable Store";
+            
         }
     }
 }
